014c809e7349bb5bab6a86ea3f14bb78d8dae855
Emin B <zSuckyLP@gmail.com>
2022-12-14 08:09:48 +0100

    change host

--- src/index.ts
+++ src/index.ts
@@ -67,7 +67,7 @@ fastify.get<{
 
 const start = async () => {
 try {
-    await fastify.listen({ port: 3000 })
+    await fastify.listen({ host: "0.0.0.0", port: 3000 })
 } catch (err) {
     fastify.log.error(err)
     process.exit(1)
